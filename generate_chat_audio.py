c11fbe391212827b223a69c56137b5c743686c7c
Richa <richashah015@gmail.com>
2025-10-25 19:14:30 -0400

    Generate Audio in actual voice -- Updated

--- generate_chat_audio.py
+++ generate_chat_audio.py
@@ -1,19 +1,10 @@
-import openai
 import os
-import wave
+from examples.generation import main as higgs_main
 import json
 
-# Make sure your BOSON_API_KEY is set
-BOSON_API_KEY = os.getenv("BOSON_API_KEY")
-
-client = openai.Client(
-    api_key=BOSON_API_KEY,
-    base_url="https://hackathon.boson.ai/v1"
-)
-
 json_file = "./chats/chat_history.json"
-transcript_file = "richa.txt"
-ref_audio = "richa.wav"
+ref_audio = "./sample_voices/richa/richa.wav"
+ref_text = './sample_voices/richa/richa.txt'
 output_folder = "chat_audio_outputs"
 os.makedirs(output_folder, exist_ok=True)
 
@@ -26,22 +17,14 @@ user_messages = [msg["text"] for msg in chat_data if msg["user"] == 'richa']
 
 for i, text in enumerate(user_messages):
     print(f"Generating audio for richa: {text}")
+    output_path = os.path.join(output_folder, f"{i}_richa.wav")
 
-    response = client.audio.speech.create(
-        model="higgs-audio-generation-Hackathon",
-        voice=ref_audio, 
-        input=text,
-        response_format="pcm"
+    higgs_main(
+        [
+        f"--transcript={text}",
+        f"--ref_audio={'richa'}",
+        f"--out_path={output_path}",
+        ]
     )
 
-    pcm_data = response.content
-
-    # Save to WAV
-    filename = os.path.join(output_folder, f"{i}_{'richa'}.wav")
-    with wave.open(filename, "wb") as wav:
-        wav.setnchannels(1)
-        wav.setsampwidth(2)
-        wav.setframerate(24000)
-        wav.writeframes(pcm_data)
-
-    print(f"Saved audio to {filename}")
+    print(f"Saved audio to {output_path}")
