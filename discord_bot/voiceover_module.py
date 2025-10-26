50dd79c1b9054423ce37789ac86dd6e9badfc2bc
lynn <rainbowkitties927@gmail.com>
2025-10-26 08:40:43 -0400

    output folder structure

--- discord_bot/voiceover_module.py
+++ discord_bot/voiceover_module.py
@@ -8,13 +8,19 @@ def voiceover_script(funny_script):
     Runs multiple_chat_audios.py -> merge_audios.py -> returns True when done.
     """
     base_dir = os.path.abspath(os.path.dirname(__file__))
-    audio_output_dir = os.path.join(base_dir, "../audio_output")
-    final_merged_audio = os.path.join(base_dir, "../audio_final/top5.wav")
+    output_dir = os.path.join(base_dir, "../outputs")
+    raw_output_dir = os.path.join(output_dir, "raw_output")
+    final_audio_dir = os.path.join(output_dir, "final_output")
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(raw_output_dir, exist_ok=True)
+    os.makedirs(final_audio_dir, exist_ok=True)
+
+    final_audio_path = os.path.join(final_audio_dir, "top5.wav")
 
     # multiple_chat_audios.run(funny_script, audio_output_dir)
     # print("[Voiceover] Starting voice generation pipeline...")
 
-    audio.merge_audios.run(audio_output_dir, final_merged_audio)
+    audio.merge_audios.run(raw_output_dir, final_audio_path)
     print("[Voiceover] Top 5 generated")
 
     return True
